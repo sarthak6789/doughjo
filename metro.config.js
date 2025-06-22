c58ba10459539009d8358679500f3616588175af
sarthak6789 <sarthakkumar1000000@gmail.com>
2025-06-22 12:29:37 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Enable React 19 JSX transform
-config.transformer.unstable_allowRequireContext = true;
-
 module.exports = config;
